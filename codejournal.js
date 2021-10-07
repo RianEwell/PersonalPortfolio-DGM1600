2e8fa7a4b73c0ae70896bbdbb05bc1b1e266b5c7
Rian Ewell <rians.ewell@gmail.com>
2021-10-07 10:51:17 -0600

    JavaScript commit

---
+++ codejournal.js
@@ -0,0 +1,27 @@
+/* Variables - containers that store values
+this is a multi-line comment
+I can keep adding lines to this*/
+
+
+var name; // a declared variable, but not initialized and it's in the global scope (BAD)
+
+let foo; // a declared variable that can be changed
+
+//const bar; //a declared variable that cannot be changed - short for 'constant'
+
+const ANSWER = 42; // const is declared and initialized with the value 42
+// Strings
+
+let string1 = "Hello World!" // this is a 'string literal' example and the preffered way - 'THis is the way'
+
+let string2 = new String("Hello World!") // using a 'constrructor'
+
+// Number
+
+let myNum = 234567787;
+
+let myDecimal = 73.4 // could also call this a 'float'
+
+// Boolean
+
+let myBool = true;
